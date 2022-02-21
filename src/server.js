dc75ed9226a823735e3c458819ffb00f991f22b7
rahul rajput <rahulrajput7358@gmail.com>
2022-02-21 16:19:13 +0530

    'test'

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const connectWithDB = require("./configs/connectWithDB");
 const app = require("./app");
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, async () => {
   try {
     await connectWithDB();
